377412d80449b432fb8d669d251833345c2f765a
Niraj <khareniraj23@gmail.com>
2026-02-01 18:21:38 +0530

    passthrough class added

--- src/jrw/module/builtin/PassThroughModule.java
+++ src/jrw/module/builtin/PassThroughModule.java
@@ -1,7 +1,12 @@
 package jrw.module.builtin;
 
+import jrw.module.JrwModule;
 
-public class PassThroughModule {
+public class PassThroughModule implements JrwModule {
 
+    @Override
+    public String process(String input){
+        return input;
+    }
     
 }
